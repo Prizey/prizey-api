8e864ffe7087a035242a9182dba8fcc256385a67
Rodrigo Avellar <roavellarm@gmail.com>
2019-04-12 11:35:14 -0300

    Add sign_uo_spec.rb

---
+++ spec/requests/auth/sign_up_spec.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'POST /auth - Sign Up', type: :request do
+  let(:user) do
+    {
+      email: 'john.connor@email.com',
+      password: '123456789'
+    }
+  end
+
+  let(:headers) { {} }
+
+  let(:params) { {} }
+
+  let(:expected_hash) do
+    {
+      id: User.last.id,
+      email: 'john.connor@email.com',
+      fullname: nil,
+      address: nil,
+      city: nil,
+      state_province_region: nil,
+      postal_code_zip: nil,
+      clothing_size: nil,
+      shoe_size: nil
+    }
+  end
+
+  context 'with valid email' do
+    before do
+      post '/auth', params: params.to_json
+    end
+
+    let(:params) { user }
+
+    it { expect(response).to have_http_status(:ok) }
+    it { expect(User.count).to eq(2) }
+
+    it 'returns the created user' do
+      expect(response.body).to eq({
+        status: 'success',
+        data: expected_hash
+      }.to_json)
+    end
+  end
+
+  context 'when the email is already taken' do
+    before do
+      User.create!(user)
+      post '/auth', params: params.to_json
+    end
+
+    let(:params) { user }
+
+    it { expect(response).to have_http_status(:unprocessable_entity) }
+
+    it 'returns error with correct messages' do
+      expect(JSON.parse(response.body)).to eq(JSON.parse({
+        status: 'error',
+        data: expected_hash.merge(id: nil),
+        errors: message_error
+      }.to_json))
+    end
+
+    it 'does NOT return access token header' do
+      expect(response.has_header?('access-token')).to eq(false)
+    end
+  end
+
+  def message_error
+    {
+      email: ['has already been taken'],
+      full_messages: ['Email has already been taken']
+    }
+  end
+end
