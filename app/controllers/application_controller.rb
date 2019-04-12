b19db011f420cf02e4ecdf08effe4ac12861ffbc
Rodrigo Avellar <roavellarm@gmail.com>
2019-04-12 11:33:24 -0300

    Install devise token auth and create User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,25 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+
+  def not_found(exception)
+    render status: :not_found, json: {
+      id: 'not_found',
+      message: exception.message
+    }
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :account_update,
+      keys: %i[fullname address city state_province_region postal_code_zip
+               clothing_size shoe_size]
+    )
+  end
 end
