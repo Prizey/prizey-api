0932df2a278fd0d6224e40ce924a4ea848f831e3
Rodrigo Avellar <roavellarm@gmail.com>
2019-04-18 14:22:21 -0300

    Update tests

--- spec/requests/game_settings/index_spec.rb
+++ spec/requests/game_settings/index_spec.rb
@@ -17,21 +17,30 @@ describe 'GET /game_settings', type: :request do
           price_multiplier: 1,
           easy_carousel_speed: 1,
           medium_carousel_speed: 2,
-          hard_carousel_speed: 3
+          hard_carousel_speed: 3,
+          easy_ticket_amount: 1,
+          medium_ticket_amount: 2,
+          hard_ticket_amount: 3
         },
         {
           id: GameSetting.second.id,
           price_multiplier: 1,
           easy_carousel_speed: 1,
           medium_carousel_speed: 2,
-          hard_carousel_speed: 3
+          hard_carousel_speed: 3,
+          easy_ticket_amount: 1,
+          medium_ticket_amount: 2,
+          hard_ticket_amount: 3
         },
         {
           id: GameSetting.third.id,
           price_multiplier: 1,
           easy_carousel_speed: 1,
           medium_carousel_speed: 2,
-          hard_carousel_speed: 3
+          hard_carousel_speed: 3,
+          easy_ticket_amount: 1,
+          medium_ticket_amount: 2,
+          hard_ticket_amount: 3
         }
       ].to_json
     end
--- spec/support/factories/game_settings.rb
+++ spec/support/factories/game_settings.rb
@@ -6,5 +6,8 @@ FactoryBot.define do
     easy_carousel_speed 1
     medium_carousel_speed 2
     hard_carousel_speed 3
+    easy_ticket_amount 1
+    medium_ticket_amount 2
+    hard_ticket_amount 3
   end
 end
