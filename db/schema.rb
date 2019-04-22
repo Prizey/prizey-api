c43facc96ead483787acb4ade4cfdebe521a03e2
Patrick Edgar Müller <patrick.e.muller@gmail.com>
2019-04-22 15:57:14 -0300

    Remove unused shopify_customer_id from User Model

--- db/schema.rb
+++ db/schema.rb
@@ -64,7 +64,6 @@ ActiveRecord::Schema.define(version: 2019_04_18_213220) do
     t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "shopify_customer_id"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
