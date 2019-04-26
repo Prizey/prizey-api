732fb87caeee3760be192281c3e8a1a0a49c46e9
Patrick Edgar Müller <patrick.e.muller@gmail.com>
2019-04-26 16:55:02 -0300

    Check if User has enough balance before TicketTransaction#create

--- app/controllers/ticket_transactions_controller.rb
+++ app/controllers/ticket_transactions_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class TicketTransactionsController < ApplicationController
+  before_action :check_user_balance, only: :create
+
   def index
     render json: current_user.ticket_transactions
   end
@@ -11,5 +13,17 @@ class TicketTransactionsController < ApplicationController
     )
 
     render json: ticket.as_json, status: :created
+  end
+
+  private
+
+  def check_user_balance
+    return true if params[:amount].to_i.positive?
+    render_error if (current_user.tickets + params[:amount].to_i).negative?
+  end
+
+  def render_error
+    render json: { id: 'error', message: 'You do not have enough Tickets' },
+           status: :unprocessable_entity
   end
 end
