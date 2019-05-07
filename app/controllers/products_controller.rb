25878eff7de77647ec23b3cc1dc23ad2245a9e37
Rodrigo Avellar <roavellarm@gmail.com>
2019-05-07 16:01:59 -0300

    Enable skip user authentication for homepage in ProductsController

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 class ProductsController < ApplicationController
+  skip_before_action :authenticate_user!, if: :homepage?
+
   def index
     collection = ShopifyCollection.find_by(:handle, params[:identifier])
     products = ShopifyCollection.products(collection['id'])
     serialized = ShopifyCollection.serialize_products(products)
     sorted = serialized.sort_by! { |product| product[:price] }
     render json: sorted, status: :ok
+  end
+
+  def homepage?
+    return true if params[:identifier] == 'homepage'
   end
 end
