424895582a332bc2b27c4f130f92a7a0c6fba66d
Patrick <patrick.e.muller@gmail.com>
2019-06-03 19:18:54 -0300

    PurchaseOptions#index endpoint was reverted to require authentication

--- app/controllers/purchase_options_controller.rb
+++ app/controllers/purchase_options_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PurchaseOptionsController < ApplicationController
-  skip_before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def index
     render json: PurchaseOption.limit(3).order(price: :asc), status: :ok
