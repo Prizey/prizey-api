1d03a0c1271e65bb4e14519d9b8d1b80a5a0123f
Giovani <sousa_giovani@hotmail.com>
2019-08-14 12:24:01 -0300

    Avoid race conditions when creating transactions

--- app/controllers/ticket_transactions_controller.rb
+++ app/controllers/ticket_transactions_controller.rb
@@ -1,35 +1,32 @@
 # frozen_string_literal: true
 
 class TicketTransactionsController < ApplicationController
-  before_action :check_user_balance, only: :create
-
   def index
     render json: current_user.ticket_transactions
   end
 
   def create
-    return render status: :forbidden if repeat_reward?
-
-    ticket = current_user.ticket_transactions.create(
-      amount: params[:amount], source: params[:source]
-    )
-
-    render json: ticket.as_json, status: :created
+    ticket = current_user.with_lock do
+      raise ActiveRecord::Rollback if invalid_transaction?
+      create_transaction
+    end
+    if ticket.present?
+      render json: ticket.as_json, status: :created
+    else
+      render_error
+    end
   end
 
   private
 
-  def repeat_reward?
-    return false if params[:source] != 'reward'
-
-    last_transaction = TicketTransaction.last
-    elapsed_time = Time.zone.now.to_i - last_transaction.created_at.to_i
-    last_transaction[:source] == 'reward' && elapsed_time < 30
+  def create_transaction
+    current_user.ticket_transactions.create(
+      amount: params[:amount], source: params[:source]
+    )
   end
 
-  def check_user_balance
-    return true if params[:amount].to_i.positive?
-    render_error if (current_user.tickets + params[:amount].to_i).negative?
+  def invalid_transaction?
+    (current_user.tickets + params[:amount].to_i).negative?
   end
 
   def render_error
