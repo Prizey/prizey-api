5214077d8e560142a9a3f5fdb2fc576ccca83cee
AlanMaik <alanmaikm@gmail.com>
2019-11-16 23:21:38 -0300

    Fix rubocop issues

--- spec/support/factories/users.rb
+++ spec/support/factories/users.rb
@@ -5,14 +5,14 @@ FactoryBot.define do
     sequence :email do |n|
       "user_#{n}@example.com"
     end
-    fullname { 'foo bar' }
-    address { 'foo bar' }
-    city { 'foobar' }
-    state_province_region { 'foobar' }
-    zipcode { 'foobar' }
-    clothing_size { 'M' }
-    shoe_size { '40' }
-    password { '123123123' }
-    stripe_customer_id { 'us_123' }
+    fullname 'foo bar'
+    address 'foo bar'
+    city 'foobar'
+    state_province_region 'foobar'
+    zipcode 'foobar'
+    clothing_size 'M'
+    shoe_size '40'
+    password '123123123'
+    stripe_customer_id 'us_123'
   end
 end
