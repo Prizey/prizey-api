e4034e3554f2cee06cf6998b22f77a452efcda29
AlanMaik <alanmaikm@gmail.com>
2019-12-08 11:16:01 -0300

    Change name of Service object

--- app/services/verify_blocked_page.rb
+++ app/services/verify_blocked_page.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
-class VerifyIpAddress
+class VerifyBlockedPage
   def self.execute(*args, &block)
     new(*args, &block).execute
   end
 
   def initialize(**keywords)
     @ip_address = keywords[:ip_address]
+    @page = keywords[:page]
   end
 
   def execute
@@ -21,12 +22,26 @@ class VerifyIpAddress
   end
 
   def find_or_create_ip_blocked
-    blocked = Blacklist.find_by(ip_address: @ip_address)
-    create_ip_blocked if blocked.blank?
+    return false unless page_valid?
+
+    blocked = Blacklist.find_by(ip_address: @ip_address, page: @page)
+    blocked = create_ip_blocked if blocked.blank?
+
     blocked.present? && blocked.created_at < 5.seconds.ago
   end
 
   def create_ip_blocked
-    Blacklist.create!(ip_address: @ip_address)
+    Blacklist.create!(ip_address: @ip_address, page: @page)
+  end
+
+  def page_valid?
+    pages = AdminText.find_by(
+      field: 'valid_free_game_pages'
+    )&.text&.delete(' ')&.split(',')
+
+    return false if pages.blank? && @page
+    return true if @page.blank?
+
+    pages.include?(@page)
   end
 end
---
+++ spec/services/verify_blocked_page_spec.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe VerifyBlockedPage, type: :service do
+  context 'when ip was not created more than 10 minutes' do
+    before do
+      Blacklist.create!(ip_address: '127.0.0.1', created_at: 10.seconds.ago)
+    end
+
+    it do
+      expect(described_class.execute(ip_address: '127.0.0.1'))
+        .to eq(success: true, payload: true)
+    end
+  end
+
+  context 'when ip was created more than 10 minutes' do
+    before do
+      Blacklist.create!(ip_address: '127.0.0.1', created_at: 11.minutes.ago)
+    end
+
+    it do
+      expect(described_class.execute(ip_address: '127.0.0.1'))
+        .to eq(success: true, payload: false)
+    end
+  end
+
+  context 'when ip was not created more than 10 minutes and page is valid' do
+    let(:create_pages) do
+      AdminText.create!(
+        field: 'valid_free_game_pages',
+        text: 'page1, page2, page3'
+      )
+    end
+
+    before do
+      create_pages
+      Blacklist.create!(
+        ip_address: '127.0.0.1', page: 'page1', created_at: 10.seconds.ago
+      )
+    end
+
+    it do
+      expect(described_class.execute(ip_address: '127.0.0.1', page: 'page1'))
+        .to eq(success: true, payload: true)
+    end
+
+    it 'with page different from the one created' do
+      expect(described_class.execute(ip_address: '127.0.0.1', page: 'page3'))
+        .to eq(success: true, payload: false)
+    end
+  end
+
+  context 'when ip was not created more than 10 minutes and page is invalid' do
+    let(:create_pages) do
+      AdminText.create!(
+        field: 'valid_free_game_pages',
+        text: 'page1, page2, page3'
+      )
+    end
+
+    before do
+      create_pages
+      Blacklist.create!(
+        ip_address: '127.0.0.1', page: 'page1', created_at: 10.seconds.ago
+      )
+    end
+
+    it do
+      expect(described_class.execute(ip_address: '127.0.0.1', page: 'page10'))
+        .to eq(success: true, payload: false)
+    end
+  end
+end
